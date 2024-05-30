5271c0e29317557aaa6c27050d2da5cc4256b36f
DanielGuzman11 <daniguzman33@gmail.com>
2024-05-30 14:55:10 -0600

    Update sidebar.blade.php

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -38,7 +38,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link {{ Request::is('correspondencia') ? 'active' : '' }}" href="{{ route('correspondencia') }}">
+                <a class="nav-link {{ Request::is('correspondencia') ? 'active' : '' }}" href="{{ url('correspondencia') }}">
                     <div
                         class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                         <svg width="12px" height="12px" viewBox="0 0 40 44" version="1.1"
