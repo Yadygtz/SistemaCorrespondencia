5425a2fcbcb86959cca70196ba5597201eadfb2c
DanielGuzman11 <daniguzman33@gmail.com>
2024-05-31 16:31:04 -0600

    Update correspondencia.blade.php

--- resources/views/correspondencia.blade.php
+++ resources/views/correspondencia.blade.php
@@ -5,6 +5,25 @@
         <div class="row">
             <div class="col-12">
                 <div class="card mb-4">
+                    <div class="card-header pb-0">
+                        @if (session('success'))
+                            <div class="alert alert-secondary alert-dismissible fade show" role="alert">
+                                <span class="alert-icon text-white"><i class="ni ni-like-2"></i></span>
+                                <span class="alert-text text-white"><strong>{{ session('success') }}</strong></span>
+                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                        @endif
+                        <div class="row">
+                            <div class="col-6 d-flex align-items-center">
+                                <h6 class="mb-0">Correspondencia</h6>
+                            </div>
+                            <div class="col-6 text-end">
+                                <a class="btn bg-gradient-danger btn-sm mb-0">Agregar</a>
+                            </div>
+                        </div>
+                    </div>
                     {{-- <div class="card-header pb-0">
                         <h6>Correspondencia</h6>
                     </div> --}}
@@ -24,6 +43,7 @@
 
                                         <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
                                             Recibido por</th>
+                                        <th class="text-center"></th>
                                         <th class="text-center"></th>
                                     </tr>
                                 </thead>
@@ -55,7 +75,6 @@
                                                 </td>
                                                 <td>
                                                     <a href="#" class="btn btn-secondary w-100 btn-icon mb-0"
-                                                        data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
                                                         data-bs-toggle="modal" data-bs-target="#detalleModal"
                                                         data-id="{{ $row->id_correspondencia }}">
                                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
@@ -66,6 +85,23 @@
                                                             <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
                                                             <path
                                                                 d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
+                                                        </svg>
+                                                    </a>
+                                                </td>
+                                                <td>
+                                                    <a href="#" class="btn btn-primary w-100 btn-icon mb-0"
+                                                        data-bs-toggle="modal" data-bs-target="#actualizarOfiModal"
+                                                        data-id="{{ $row->id_correspondencia }}">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                            viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                            class="icon icon-tabler icons-tabler-outline icon-tabler-edit">
+                                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                            <path
+                                                                d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" />
+                                                            <path
+                                                                d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" />
+                                                            <path d="M16 5l3 3" />
                                                         </svg>
                                                     </a>
                                                 </td>
@@ -311,15 +347,149 @@
                     <p><strong>Recibido Por:</strong> <span id="modal_recibido_por"></span></p>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                    <button type="button" class="btn bg-gradient-secondary" data-bs-dismiss="modal">Cerrar</button>
                 </div>
             </div>
         </div>
     </div>
+
+    {{-- Modal Add/Upd Oficio --}}
+    <div class="modal fade" id="detalleModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="detalleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="detalleModalLabel">Agregar Oficio</h5>
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Guardar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="actualizarOfiModal" data-bs-keyboard=false data-bs-backdrop="static" tabindex="-1">
+        <div class="modal-dialog modal-lg modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Oficio</h5>
+                    <button type="button" wire:click.prevent="cancel" class="btn-close"
+                        data-bs-dismiss="modal"></button>
+                    <div class="modal-status bg-primary border-wide"></div>
+                </div>
+                <form action="correspondencia/upd/id_correspondencia" method="POST" id="formUpdOfi">
+                    @csrf
+                    @method('PUT')
+                    <div class="modal-body">
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <label class="form-label required">No. Oficio</label>
+                                <input type="text" class="form-control @error('no_oficio') is-invalid @enderror"
+                                    name="no_oficio" id="no_oficio" required>
+                                @error('no_oficio')
+                                    <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div class="col-md-6">
+                                <label class="form-label">Fecha de Oficio</label>
+                                <input type="date" class="form-control @error('fecha_oficio') is-invalid @enderror"
+                                    name="fecha_oficio" id="fecha_oficio">
+                                @error('fecha_oficio')
+                                    <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <label class="form-label">Enviado por</label>
+                                <input type="text" class="form-control @error('enviado_por') is-invalid @enderror"
+                                    name="enviado_por" id="enviado_por" required>
+                                @error('enviado_por')
+                                    <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div class="col-md-6">
+                                <label class="form-label">Asunto</label>
+                                <input type="text" class="form-control @error('asunto') is-invalid @enderror"
+                                    name="asunto" id="asunto" required>
+                                @error('asunto')
+                                    <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <label class="form-label">Recibido por</label>
+                                <input type="text" class="form-control @error('recibido_por') is-invalid @enderror"
+                                    name="recibido_por" id="recibido_por" required>
+                                @error('recibido_por')
+                                    <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div class="col-md-6">
+                                <label class="form-label">Fecha de Recibido</label>
+                                <input type="date" class="form-control @error('fecha_recibido') is-invalid @enderror"
+                                    name="fecha_recibido" id="fecha_recibido">
+                                @error('fecha_recibido')
+                                    <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label class="form-label @error('area') is-invalid @enderror">Área</label>
+                                <select class="form-select" {{ $areaCB_activo ? '' : 'disabled' }} name="areaCB"
+                                    id="areaCB" required>
+                                    <option value=""></option>
+                                    {{!! $areasCB !!}}
+                                </select>
+                                @error('area')
+                                    <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div class="col-md-6">
+                                <label class="form-label">Folder</label>
+                                <input type="text" class="form-control @error('folder') is-invalid @enderror"
+                                    name="folder" id="folder">
+                                @error('folder')
+                                    <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" wire:click.prevent="cancel" class="btn bg-gradient-secondary me-auto"
+                            data-bs-dismiss="modal">
+                            Cerrar
+                        </button>
+                        <button type="submit" class="btn bg-gradient-primary">
+                            GUARDAR
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
 @endsection
 @push('scripts')
     <script>
         window.onload = function() {
+
+            $('#areaCB').val(@json($area));
+
+            // alert(@json(date('Y-m-d')));
+
             var confidioma = {
                 "info": "<span class='text-sm text-secondary opacity-9'>Mostrando _START_ a _END_ de _TOTAL_ registros</span>",
                 "infoEmpty": "Mostrando 0 a 0 de 0 registros",
@@ -362,6 +532,36 @@
                         $('#modal_recibido_por').text(data.recibido_por);
                     }
                 });
+            });
+
+            $('#actualizarOfiModal').on('show.bs.modal', function(event) {
+                var button = $(event.relatedTarget);
+                var id = button.data('id');
+
+                $("#formUpdOfi").attr('action', 'correspondencia/upd/' + id);
+                $.ajax({
+                    url: '/correspondencia/' + id,
+                    method: 'GET',
+                    success: function(data) {
+                        $("#no_oficio").val(data.no_oficio);
+                        $("#fecha_oficio").val(data.fecha_oficio);
+                        $("#enviado_por").val(data.enviado_por);
+                        $("#asunto").val(data.asunto);
+                        $("#recibido_por").val(data.recibido_por);
+                        $("#fecha_recibido").val(data.fecha_recibido);
+                        $("#areaCB").val(data.area);
+                        $("#folder").val(data.folder);
+                    }
+                });
+            });
+
+
+            $('#detalleModal').on('hide.bs.modal', function(event) {
+                $('#modal_no_oficio').text('');
+                $('#modal_asunto').text('');
+                $('#modal_fecha_oficio').text('');
+                $('#modal_enviado_por').text('');
+                $('#modal_recibido_por').text('');
             });
         }
     </script>
