3f4889c2aa8de26daaf95c80496af9b9e00c27d9
DanielGuzman11 <daniguzman33@gmail.com>
2024-06-17 09:21:54 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\CorrespondenciaController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Response;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,7 +24,9 @@ Auth::routes();
 
 
 Route::middleware('auth')->group(function () {
-    Route::get('/', function(){ return redirect('/correspondencia'); });
+    Route::get('/', function () {
+        return redirect('/correspondencia');
+    });
 
     Route::get('/correspondencia', [CorrespondenciaController::class, 'index'])->name('correspondencia');
     // Route::resource('correspondencia', CorrespondenciaController::class);
@@ -34,4 +38,24 @@ Route::middleware('auth')->group(function () {
     Route::get('dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    /* OBTENER EL ARCHIVO DEL SERVER FTP*/
+    // Expediente digital
+    Route::get('veroficio/{oficio}', function ($oficio) {
+        // Verificar en cuál de los dos servidores se encuentra el fichero
+        $rutaCompletaFTP = "Oficios/" . $oficio . ".pdf";
+        if (Storage::disk('ftp')->exists($rutaCompletaFTP)) {
+            if (ob_get_level()) ob_end_clean();
+            $file = Storage::disk('ftp')->get($rutaCompletaFTP);
+
+            $response = Response::make($file, 200);
+            $response->header('Content-Type', 'application/pdf');
+
+            return $response;
+        } else {
+            return "Archivo no encontrado";
+        }
+
+        return $oficio;
+    })->name('veroficio');
 });
