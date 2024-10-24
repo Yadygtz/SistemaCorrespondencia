b860a1be8d6429fac817f2b1e81218ac828362f4
Yadira Gutiérrez Méndez <ygutierrez@c3.tam>
2024-10-24 11:11:07 -0600

    agrego layout a la vista editar usuario

--- resources/views/auth/registro.blade.php
+++ resources/views/auth/registro.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.app')
-
-@section('guest')
+@extends('layouts.user_type.auth')
+@section('titulo','Usuarios del Sistema')
+@section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
